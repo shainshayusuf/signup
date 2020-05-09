739b603d58f7cb4c9e277440b4ead0cae4d3378b
shainshayusuf <55499261+shainshayusuf@users.noreply.github.com>
2020-05-09 22:03:06 +0530

    Add files via upload

--- model/user.js
+++ model/user.js
@@ -1,1 +1,28 @@
+const mongoose = require('mongoose');
 
+
+//cretae a schema
+const UserSchema = new mongoose.Schema({
+fname:{
+  type:String,
+  required:true
+},
+lname:{
+    type:String,
+    required:true
+  },
+email:{
+type:String,
+required:true
+}, 
+otp:{
+    type:Number,
+},
+status:{
+    type:String,
+},
+password:{
+    type:String,
+    }, 
+}); 
+ module.exports=mongoose.model('users',UserSchema); 
